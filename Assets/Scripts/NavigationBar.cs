27e4b18a8c36d71ba774bda119475a80afbe9f45
SanBUnity <bharathy.gs@kevells.com>
2021-07-13 19:05:13 +0530

    second commit

---
+++ Assets/Scripts/NavigationBar.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class NavigationBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject groundedGreen;
+    public GameObject MaskLine;
+    public GameObject Rounded;
+
+    public GameObject[] naviBut;
+    GameObject Gu;
+    void Start()
+    {
+        Gu = naviBut[0]; 
+    }
+
+    public void OnNaviClick()
+    {
+        Gu.SetActive(true);
+        Gu = EventSystem.current.currentSelectedGameObject;
+        Rounded.transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().sprite =
+        Gu.transform.GetChild(0).GetComponent<Image>().sprite;
+        Gu.SetActive(false);
+
+        //Debug.Log(Gu.name+""+Gu.GetComponent<RectTransform>().anchoredPosition);
+        //groundedGreen.GetComponent<RectTransform>().anchoredPosition = 
+        //    new Vector2(
+        //    Gu.GetComponent<RectTransform>().anchoredPosition.x,
+        //    groundedGreen.GetComponent<RectTransform>().anchoredPosition.y);
+
+        groundedGreen.GetComponent<RectTransform>().DOAnchorPosX(
+            Gu.GetComponent<RectTransform>().anchoredPosition.x, 0.25f, true);
+        MaskLine.GetComponent<RectTransform>().DOAnchorPosX(
+            Gu.GetComponent<RectTransform>().anchoredPosition.x - 160, 0.25f, true);
+        Rounded.transform.GetChild(0).transform.localScale = Vector3.zero;
+        Rounded.GetComponent<RectTransform>().DOAnchorPosX(
+            Gu.GetComponent<RectTransform>().anchoredPosition.x - 160, 0.25f, true).
+            OnComplete(() => { OnenEnd();});
+    }
+
+    void OnenEnd()
+    {
+        //Rounded.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector2(0,21);
+        //Rounded.transform.GetChild(0).GetComponent<RectTransform>().DOAnchorPosY(85,0.15f);
+        Rounded.transform.GetChild(0).transform.DOScale(Vector3.one,0.25f);
+    }
+
+    void sendDow(GameObject doe)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class UISCreen
+{
+    public string Name;
+    public int ID;
+    public GameObject UiPanel;
+    public List<UIscreenCont> objectInScene;
+}
+
+[System.Serializable]
+public class UIscreenCont
+{
+    public string id;
+    public GameObject UiObj;
+}
+public class UIManager : MonoBehaviour
+{
+    public List<UISCreen> Screens;
+    public GameObject MenuBar;
+
+    // Start is called before the first frame update
+    public void StartScreenscroll()
+    {
+        Debug.Log("Call it"+ Screens[0].objectInScene[3].UiObj.GetComponent<RectTransform>().anchoredPosition);
+    }
+    public void SplashtoLogin()
+    {
+        clearScree();
+        ScreenTrans(0,1,false);
+    }
+    public void Logintosignup(bool swap)
+    {
+        ScreenTrans(1, 2, swap);
+    }
+
+    public void signuptoVerifyEmail(bool swap)
+    {
+        ScreenTrans(2, 3, swap);
+    }
+    public void forgotPassword(bool swap)
+    {
+        ScreenTrans(1, 4, swap);
+    }
+    public void VerForgotOTP(bool swap)
+    {
+        ScreenTrans(4, 5, swap);
+    }
+    public void ResetPass(bool swap)
+    {
+        ScreenTrans(5, 6, swap);
+    }
+    public void avatarSelection()
+    {
+        if (Screens[2].objectInScene[0].UiObj.activeInHierarchy)
+        {
+            Screens[2].objectInScene[0].UiObj.SetActive(false);
+        }
+        else
+        {
+            Screens[2].objectInScene[0].UiObj.SetActive(true);
+        }
+    }
+    public void PrivateRegister()
+    {
+        if (Screens[8].objectInScene[0].UiObj.activeInHierarchy)
+        {
+            Screens[8].objectInScene[0].UiObj.SetActive(false);
+        }
+        else
+        {
+            Screens[8].objectInScene[0].UiObj.SetActive(true);
+        }
+    }
+    public void DashBoard()
+    {
+        if (!Screens[7].UiPanel.activeInHierarchy)
+        {
+            clearScree();
+            Screens[7].UiPanel.SetActive(true);
+            MenuBar.SetActive(true);
+        }
+    }
+    public void DashBoardtoTour()
+    {
+        if (!Screens[8].UiPanel.activeInHierarchy)
+        {
+            clearScree();
+            Screens[8].UiPanel.SetActive(true);
+            MenuBar.SetActive(true);
+        }
+    }
+    public void rewardsScreen()
+    {
+        if (!Screens[9].UiPanel.activeInHierarchy)
+        {
+            clearScree();
+            Screens[9].UiPanel.SetActive(true);
+            MenuBar.SetActive(true);
+        }
+    }
+    public void AddCoin()
+    {
+        if (!Screens[14].UiPanel.activeInHierarchy)
+        {
+            clearScree();
+
+            Screens[14].UiPanel.SetActive(true);
+            MenuBar.SetActive(false);
+        }
+    }
+    public void LeaderScore()
+    {
+        if (!Screens[11].UiPanel.activeInHierarchy)
+        {
+            clearScree();
+            
+            Screens[11].UiPanel.SetActive(true);
+            MenuBar.SetActive(false);
+        }
+        Screens[11].objectInScene[0].UiObj.SetActive(true);
+        Screens[11].objectInScene[1].UiObj.SetActive(false);
+    }
+    public void FriendScreen()
+    {
+        if (!Screens[10].UiPanel.activeInHierarchy)
+        {
+            clearScree();
+            Screens[10].UiPanel.SetActive(true);
+            MenuBar.SetActive(true);
+        }
+    }
+    public void LeaderBoard()
+    {
+        if (!Screens[11].objectInScene[1].UiObj.activeInHierarchy)
+        {
+            //clearScree();
+            Screens[11].objectInScene[1].UiObj.SetActive(true);
+            Screens[11].objectInScene[0].UiObj.SetActive(false);
+            //MenuBar.SetActive(true);
+        }
+    }
+    void ScreenTrans(int from, int to, bool swap)
+    {
+        if (!swap)
+        {
+            Screens[from].UiPanel.SetActive(false);
+            Screens[to].UiPanel.SetActive(true);
+        }
+        else
+        {
+            Screens[to].UiPanel.SetActive(false);
+            Screens[from].UiPanel.SetActive(true);
+        }
+    }
+    void clearScree()
+    {
+        foreach (UISCreen n in Screens)
+        {
+            if (n.UiPanel.activeInHierarchy)
+            {
+                n.UiPanel.SetActive(false);
+            }
+        }
+    }
+}
